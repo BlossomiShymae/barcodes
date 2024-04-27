a57692b548e8eb8b8c1595091bba0669f2c068e7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2024-04-27 13:14:43 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,34 @@ pub const TYPE_SORTS: [&str; 4] = ["█", "▊", "▌", "▎"];
 fn main() {
     let cli = Cli::parse();
     let colors = cli.command.colors();
+    let colors_length = 2;
 
     let size = termsize::get().unwrap();
-
     let length = colors.len() as u16;
     let chunk = size.cols / length;
+
+    let mut buffer = String::from("");
 
     for _ in 0..length {
         for _ in 0..chunk {
             let color = *colors.choose(&mut thread_rng()).unwrap();
-            print!(
-                "{}",
-                TYPE_SORTS
-                    .choose(&mut thread_rng())
-                    .unwrap()
-                    .color(RGB::from(color))
+            buffer.push_str(
+                format!(
+                    "{}",
+                    TYPE_SORTS
+                        .choose(&mut thread_rng())
+                        .unwrap()
+                        .color(RGB::from(color))
+                )
+                .as_str(),
             )
         }
     }
-    println!("");
+
+    let line = buffer.clone();
+    for _ in 0..colors_length - 1 {
+        buffer.push_str(line.as_str());
+    }
+
+    println!("{buffer}");
 }
